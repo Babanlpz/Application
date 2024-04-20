58c0d7a0562c3f9983ee1834ccc62e41c9c410ab
Esteban <baab2an@live.fr>
2024-04-20 11:30:45 +0200

    Feat form sur la modification de contact

--- src/components/AddAndUpdate.jsx
+++ src/components/AddAndUpdate.jsx
@@ -113,7 +113,7 @@ const AddAndUpdate = ({ openModal, onClose, contact, isUpdate }) => {
                 type="submit"
                 className="bg-teal-500 px-3 py-1.5 text-white my-3 self-end"
               >
-                {isUpdate ? "Modifier le" : "Ajouter un"}contact
+                {isUpdate ? "Modifier le " : "Ajouter un "}contact
               </button>
             </Form>
           </Formik>
